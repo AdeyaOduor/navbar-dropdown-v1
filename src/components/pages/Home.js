e20f1462ec644ccabb4c20488822a09214d334c0
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2023-09-24 00:39:35 +0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import '../../App.css';
 export default function Home() {
   return (
     <>
-      <h1 className='home'>Login</h1>
+      <h1 className='home'>Welcome to my Page</h1>
     </>
   );
 }
